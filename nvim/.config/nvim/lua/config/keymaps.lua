d5ee0266035735774375fe14b952be5c854023d5
GiannisChouliaras <giannis.chouliaras10@gmail.com>
2024-11-28 08:59:14 +0200

    keymaps for obsidian

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -53,18 +53,17 @@ keymap.set("v", ";a", "<ESC>", opts)
 --- navigate to vault
 vim.keymap.set(
     "n",
-    "<leader>on",
+    "son",
     ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>",
     { desc = "Create new note with template" }
 )
 vim.keymap.set(
     "n",
-    "<leader>ok",
+    "sok",
     ":!mv '%:p' /home/ioannis-chouliaras/Documents/second_brain/brain<cr>:bd<cr>",
     { desc = "Move file to brain directory" }
 )
-vim.keymap.set("n", "<leader>odd", ":!rm '%:p'<cr>:bd<cr>", { desc = "Delete current file" })
-
+vim.keymap.set("n", "sod", ":!rm '%:p'<cr>:bd<cr>", { desc = "Delete current file" })
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
     vim.diagnostic.goto_next()
